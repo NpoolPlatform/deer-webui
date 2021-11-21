ab82f530b3574a04bf86f2ea0a1df2cd2ec204d8
Zhao KK <kikakkz@hotmail.com>
2021-11-22 01:51:18 +0800

    Debug get goods

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -25,25 +25,28 @@
     </div>
     <div class="container">
       <h4 class="title">{{ $t('BUTTON.PLATFORM_RECOMMEND') }}</h4>
-      <q-list class="recommend-list">
+      <!-- q-list class="recommend-list">
         <recommend-good-card
           v-for="recommend in recommends"
-          :key="recommend.id"
+          :key="recommend.ID"
           :good="recommend">
         </recommend-good-card>
-      </q-list>
+      </q-list -->
     </div>
   </div>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue'
-import RecommendGoodCard from '../components/RecommendGoodCard.vue'
+// import RecommendGoodCard from '../components/RecommendGoodCard.vue'
+import { api } from 'boot/axios'
+import { fail } from '../notify/notify'
+import mitt from 'mitt'
 
 export default defineComponent({
   name: 'PageIndex',
   components: {
-    RecommendGoodCard
+    // RecommendGoodCard
   },
   setup () {
     return {
@@ -67,146 +70,83 @@ export default defineComponent({
           url: 'http://localhost:8080',
         }
       ],
-      recommends: [
-        {
-          id: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaa",
-          cointype: 'BTC',
-          supportedCoinTypes: ['BTC', 'BCH'],
-          coinlogo: 'logo/btc.png',
-          title: '蚂蚁矿机S19Pro套餐',
-          rating: 4.2,
-          rateCount: 529,
-          duration: 180,
-          unit: 'TH/s',
-          amount: 200,
-          posters: ['logo/btc.png', 'logo/btc.png', 'logo/btc.png', 'logo/btc.png'],
-          gotourl: 'http://localhost:8080',
-          fees: [
-            {
-              type: '技术服务费',
-              paytype: 'percent',
-              percent: 10,
-            }, {
-              type: '电费',
-              paytype: 'amount',
-              unitamount: 0.05,
-              currency: 'USDT'
-            }
-          ],
-          start: Math.round(+new Date()/1000),
-          badges: ['新币种', '热卖', '即将售罄', '自营'],
-          outsale: false,
-          presale: false,
-          price: 2029.70,
-          priceunit: 'USDT',
-          pricecurrency: 'USDT',
-          pricecurrencychar: '$'
+      recommends: [{
+        Actuals: true,
+        ID: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaa",
+        BenefitType: "platform",
+        CoinInfo: {
+          ID: "aaaaaaaa-bbbb-aaaa-aaaa-aaaaaaaa",
+          CoinType: "BTC",
+          CoinLogo: 'logo/btc.png'
+        },
+        SupportedCoinTypes: [{
+          ID: "aaaaaaaa-bbbb-dddd-aaaa-aaaaaaaa",
+          CoinType: "BTC",
+          CoinLogo: 'logo/btc.png'
         }, {
-          id: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaab",
-          cointype: 'BTC',
-          supportedCoinTypes: ['BTC', 'BCH'],
-          coinlogo: 'logo/btc.png',
-          title: '蚂蚁矿机S20Pro套餐',
-          rating: 4.2,
-          rateCount: 529,
-          duration: 180,
-          unit: 'TH/s',
-          amount: 200,
-          posters: ['logo/btc.png', 'logo/btc.png', 'logo/btc.png', 'logo/btc.png'],
-          gotourl: 'http://localhost:8080',
-          fees: [
-            {
-              type: '技术服务费',
-              paytype: 'percent',
-              percent: 10,
-            }, {
-              type: '电费',
-              paytype: 'amount',
-              unitamount: 0.05,
-              currency: 'USDT'
-            }
-          ],
-          start: Math.round(+new Date()/1000),
-          badges: ['热卖', '自营'],
-          outsale: false,
-          presale: false,
-          price: 1029.70,
-          priceunit: 'USDT',
-          pricecurrency: 'USDT',
-          pricecurrencychar: '$'
-        }, {
-          id: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaac",
-          cointype: 'BTC',
-          supportedCoinTypes: ['BTC', 'BCH'],
-          coinlogo: 'logo/btc.png',
-          title: '蚂蚁矿机S21Pro套餐',
-          rating: 4.2,
-          rateCount: 529,
-          duration: 180,
-          unit: 'TH/s',
-          amount: 200,
-          posters: ['logo/btc.png', 'logo/btc.png', 'logo/btc.png', 'logo/btc.png'],
-          gotourl: 'http://localhost:8080',
-          fees: [
-            {
-              type: '技术服务费',
-              paytype: 'percent',
-              percent: 10,
-            }, {
-              type: '电费',
-              paytype: 'amount',
-              unitamount: 0.05,
-              currency: 'USDT'
-            }
-          ],
-          start: Math.round(+new Date()/1000),
-          badges: ['热卖', '自营'],
-          outsale: false,
-          presale: false,
-          price: 1129.70,
-          priceunit: 'USDT',
-          pricecurrency: 'USDT',
-          pricecurrencychar: '$'
-        }, {
-          id: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaad",
-          cointype: 'BTC',
-          supportedCoinTypes: ['BTC', 'BCH'],
-          coinlogo: 'logo/btc.png',
-          title: '蚂蚁矿机S22Pro套餐',
-          rating: 4.2,
-          rateCount: 529,
-          duration: 180,
-          unit: 'TH/s',
-          amount: 200,
-          posters: ['logo/btc.png', 'logo/btc.png', 'logo/btc.png', 'logo/btc.png'],
-          gotourl: 'http://localhost:8080',
-          fees: [
-            {
-              type: '技术服务费',
-              paytype: 'percent',
-              percent: 10,
-            }, {
-              type: '电费',
-              paytype: 'amount',
-              unitamount: 0.05,
-              currency: 'USDT'
-            }
-          ],
-          start: Math.round(+new Date()/1000),
-          badges: ['预售'],
-          outsale: false,
-          presale: false,
-          price: 1229.70,
-          priceunit: 'USDT',
-          pricecurrency: 'USDT',
-          pricecurrencychar: '$'
+          ID: "aaaaaaaa-bbbb-cccc-aaaa-aaaaaaaa",
+          CoinType: "BCH",
+          CoinLogo: 'logo/btc.png'
+        }],
+        Title: '蚂蚁矿机S19Pro套餐',
+        Extra: {
+          Rating: 4.2,
+          VoteCount: 529,
+          Posters: ['logo/btc.png', 'logo/btc.png', 'logo/btc.png', 'logo/btc.png'],
+          Labels: ['新币种', '热卖', '即将售罄', '自营'],
+          OutSale: false,
+          PreSale: false,
+        },
+        DurationDays: 180,
+        Unit: 'TH/s',
+        UnitPower: 200,
+        Fees: [
+          {
+            ID: "aaaaaaaa-bbbb-cccc-aaaa-aaaaaaab",
+            FeeType: '技术服务费',
+            PayType: 'percent',
+            PercentValue: 10,
+          }, {
+            ID: "aaaaaaaa-bbbb-cccc-aaaa-aaaaaaac",
+            FeeType: '电费',
+            PayType: 'amount',
+            AmountValue: 0.05,
+          }
+        ],
+        Start: Math.round(+new Date()/1000),
+        Price: 2029.70,
+        pricecurrency: {
+          ID: "aaaaaaaa-bbbb-cccc-aaaa-aaaaaaae",
+          Name: "USDT",
+          Unit: "USDT",
+          Symbol: "$"
         }
-      ]
+      }],
+      emitter: mitt()
     }
+  },
+  created() {
+    this.getRecommendGoods()
+    this.emitter.on('recommend_goods_received', this.onRecommendGoodsReceived)
   },
   methods: {
     onBannerSliderClick: function(url) {
 
+    },
+    getRecommendGoods: function () {
+      var thiz = this
+
+      api.post('/cloud-hashing-goods/v1/get/goods/detail')
+      .then(function (resp) {
+        thiz.emitter.emit('recommend_goods_received', resp.data.Details)
+      })
+      .catch(function (error) {
+        fail(undefined, thiz.$t('GENERAL.FAIL_SIGNIN'), error, error)
+      })
+    },
+    onRecommendGoodsReceived: function (recommends) {
+      console.log(recommends)
+      this.recommends = recommends
     }
   }
 })
